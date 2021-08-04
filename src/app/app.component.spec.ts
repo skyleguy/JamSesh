5643d618037eb30b789216cb88c1275add5895ae
Skyleguy <kylelobsinger@gmail.com>
2021-08-04 14:07:23 -0400

    convert TestBed -> spectator test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,31 +1,30 @@
-import { TestBed } from '@angular/core/testing';
+/* Node Modules */
 import { RouterTestingModule } from '@angular/router/testing';
+import { createComponentFactory, Spectator } from '@ngneat/spectator/jest';
+
+/* Components */
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let spectator: Spectator<AppComponent>;
+  const createComponent = createComponentFactory({
+    component: AppComponent,
+    imports: [RouterTestingModule]
+  });
+
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent]
-    }).compileComponents();
+    spectator = createComponent();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(spectator.component).toBeTruthy();
   });
 
   it(`should have as title 'ionic-base'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('ionic-base');
+    expect(spectator.component.title).toEqual('ionic-base');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ionic-base app is running!');
+    expect(spectator.query('.content span')?.textContent).toContain('ionic-base app is running!');
   });
 });
